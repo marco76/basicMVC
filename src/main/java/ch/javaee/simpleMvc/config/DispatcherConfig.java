8a067d60d7af53191a762602d77723a144e76933
moltenma@gmail.com <moltenma@gmail.com>
2013-11-02 20:55:19 +0100

    Initial commit

---
+++ src/main/java/ch/javaee/simpleMvc/config/DispatcherConfig.java
@@ -0,0 +1,20 @@
+package ch.javaee.simpleMvc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@Import({ThymeleafConfig.class})
+public class DispatcherConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/html/**").addResourceLocations("/html/");
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+    }
+
+}
---
+++ src/main/java/ch/javaee/simpleMvc/config/ThymeleafConfig.java
@@ -0,0 +1,42 @@
+package ch.javaee.simpleMvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring3.SpringTemplateEngine;
+import org.thymeleaf.spring3.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
+
+@Configuration
+@PropertySource("classpath:thymeleaf.properties")
+public class ThymeleafConfig {
+
+
+    @Bean
+    public TemplateResolver templateResolver(){
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public ViewResolver viewResolver(){
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver() ;
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setOrder(1);
+
+        return viewResolver;
+    }
+}
