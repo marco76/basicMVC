4f226fcdde0f28665eda502e3b90db87a24998ef
moltenma@gmail.com <moltenma@gmail.com>
2013-11-30 18:54:34 +0100

    init

--- src/main/java/ch/javaee/simpleMvc/config/AppSecurityWebInitializer.java
+++
@@ -1,8 +0,0 @@
-package ch.javaee.simpleMvc.config;
-
-import org.springframework.core.annotation.Order;
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-@Order(2)
-public class AppSecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
-}
